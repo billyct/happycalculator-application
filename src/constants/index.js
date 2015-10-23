f049031a64f13e967416551db7df2eb4e2d71d16
billyct <billy_allen@126.com>
2015-10-23 17:32:13 +0800

    解决在gh-pages的不能直接加载/xxx/xx的问题

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,7 @@
 import {uuid} from '../helpers';
 import Language, {languages}  from './language';
 
-export const SVG_URL = '/dist/icons/sprites.svg';//这个是根据gulp生成svg来做的静态的svg地址
+export const SVG_URL = 'http://billyct.github.io/happycalculator-application/dist/icons/sprites.svg';//这个是根据gulp生成svg来做的静态的svg地址
 export const COMMON_STYLE_CLASS = 'happy'; //common style name
 export const LOCAL_STORAGE_KEY = 'happycalculator';
 export const THE_INIT_FORMULAS = [
